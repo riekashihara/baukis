90c00bbe45863b0d2dd71e5ff04024a804c3649b
樫原理映子 <riekokashihara@kashiharaosamueikonoMacBook.local>
2018-03-27 00:03:42 +0900

    タイムゾーンとロケールの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,16 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Baukis
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # タイムゾーンを東京に設定
+    config.time_zone = 'Tokyo'
+    # ロケールファイルのロードパスを設定（デフォルトロケールを「日本語」に設定）
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb.yml}').to_s]
+    config.i18n.default_locale = :ja
+    # config.load_defaults 5.1
   end
 end
