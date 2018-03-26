eca35fcabc6309edae60f243943f2d309e05b054
樫原理映子 <riekokashihara@kashiharaosamueikonoMacBook.local>
2018-03-27 00:11:00 +0900

    ジェネレータのソースコード自動生成しないようにOFFにした

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,15 @@ module Baukis
       Dir[Rails.root.join('config', 'locales', '**', '*.{rb.yml}').to_s]
     config.i18n.default_locale = :ja
     # config.load_defaults 5.1
+
+    # テストフレームワークをRspecに変更
+    # ジェネレータによるソースコードの自動生成をOff
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework: rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
