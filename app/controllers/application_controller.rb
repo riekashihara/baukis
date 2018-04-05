9a6c0bb7da2e10a136016c37b3e01f1883a285b7
樫原理映子 <riekokashihara@kashiharaosamueikonoMacBook.local>
2018-04-05 12:13:40 +0900

    色を変数で定義すためのパーシャルを追加・色変数を定義、各CSS内で色の変数に書き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   private
   def set_layout
-    if parmas[:controller].match(%r{\A(staff|admin|customer)/})
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       'customer'
