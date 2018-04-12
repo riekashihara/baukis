f3e9d8258eae4e1fb2028b1713c478e2a6e33860
樫原理映子 <riekokashihara@kashiharaosamueikonoMacBook.local>
2018-04-12 13:42:37 +0900

    エラー500、403、404のビューを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,36 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   layout :set_layout
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  rescue_from Exception, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActionController::RoutingError, with: :rescue404
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private
   def set_layout
-    if params[:controller].match(%r{\A(staff|admin|customer)/})
+    if request.path.match(%r{/(staff|admin|customer)\b})
       Regexp.last_match[1]
     else
       'customer'
     end
+  end
+
+  def rescue403(e)
+    @exception = e
+    render 'errors/forbidden', status: 403
+  end
+
+  def rescue404(e)
+    @exception = e
+    render 'errors/not_found', status: 404
+  end
+
+  def rescue500(e)
+    @exception = e
+    render 'errors/internal_server_error', status: 500
   end
 end
